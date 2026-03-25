57a411dbf3478399680569fe804d99281673ef95
tekken929 <90942139+tekken929@users.noreply.github.com>
2026-03-25 05:16:10 -0400

    Updated Welcome.tsx

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -93,7 +93,7 @@ export function Welcome() {
             <h2 className="text-3xl font-bold text-gray-900 dark:text-white">Music JukeBox</h2>
           </div>
           <p className="text-lg text-gray-700 dark:text-gray-300 leading-relaxed mb-6">
-            Enjoy music and songs while you explore. The music player will continue playing as you navigate through the site. All of these songs are originally produced and quote the Psalms.
+            Enjoy music and songs while you explore. The music player will continue playing as you navigate through the site. All of these songs are originals and come from the Book of Psalms.
           </p>
           <Link
             to="/music"
